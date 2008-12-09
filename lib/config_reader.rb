0902cb879672145e819a3fd320e2105359d61a5a
Michael Moen <michael@underpantsgnome.com>
2008-12-08 18:01:03 -0600

    fix the environment merge for real this time

--- lib/config_reader.rb
+++ lib/config_reader.rb
@@ -25,7 +25,7 @@ class ConfigReader
     def reload
       raise 'No config file set' unless @config_file
 
-      if defined?(ERB) 
+      if defined?(ERB)
         conf = YAML.load(ERB.new(File.open(find_config).read).result)
       else
         conf = YAML.load(File.open(find_config).read)
@@ -40,26 +40,28 @@ class ConfigReader
       end
 
       _conf = conf['defaults']
-      _conf.merge(conf[env]) if conf[env]
+      _conf.merge!(conf[env]) if conf[env]
       _conf
     end
 
     def [](key)
       config[key]
     end
-  
+
     def find_config
+      return @config_file if File.exist?(@config_file)
+
       %w( . config ).each do |dir|
         config_file = File.join(dir, @config_file)
         return config_file if File.exist?(config_file)
       end
       ''
-    end  
+    end
 
     def method_missing(key)
       config[key.to_s] || super
     end
-  
+
     def inspect
       puts config.inspect
     end
--- lib/config_reader/version.rb
+++ lib/config_reader/version.rb
@@ -2,7 +2,7 @@ module ConfigReader
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 0
-    TINY  = 1
+    TINY  = 4
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
