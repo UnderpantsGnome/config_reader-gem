1ccbc17683cb91ad26ff7e44036be4dc5b9e5296
Michael Moen <michael@underpantsgnome.com>
2023-03-11 15:16:52 -0600

    update for Ruby 3.2 support; remove 3.2 from matrix until Sekrets is updated

--- spec/sekrets_config.rb
+++ spec/sekrets_config.rb
@@ -1,7 +1,11 @@
 class SekretsConfig < ConfigReader
+  secret_file = RUBY_VERSION > '3.1' ?
+    'spec/sekrets_config.yml.enc' :
+    'spec/sekrets_ruby_3_2.yml.enc'
+
   configure do |config|
     config.environment = 'test'
     config.config_file = 'spec/test_config.yml'
-    config.sekrets_file = 'spec/sekrets_config.yml.enc'
+    config.sekrets_file = secret_file
   end
 end
